aa135d456fa2ac91ae2983040e22567483f7160f
Serdar Karacay <csharp221@gmail.com>
2020-05-20 16:57:45 +0300

    totalamountpoints :hamster:

---
+++ Cafe/totalamountpoints.js
@@ -0,0 +1,113 @@
+function points(games) {
+  const firstValue = 0;
+  return games.reduce((i, v) => {
+    const [home, away] = v.split(":");
+    debugger;
+    if (home > away) return i + 3;
+    if (home == away) return i + 1;
+    return i;
+  }, firstValue);
+}
+
+console.log(points([
+    "1:0",
+    "2:0",
+    "3:0",
+    "4:0",
+    "2:1",
+    "3:1",
+    "4:1",
+    "3:2",
+    "4:2",
+    "4:3",
+  ]))
+
+
+const Test = require("chai").assert;
+describe("Basic tests", function () {
+  it("Testing for points(['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3'])", function () {
+    Test.strictEqual(
+      points([
+        "1:0",
+        "2:0",
+        "3:0",
+        "4:0",
+        "2:1",
+        "3:1",
+        "4:1",
+        "3:2",
+        "4:2",
+        "4:3",
+      ]),
+      30
+    );
+  });
+  it("Testing for points(['1:1','2:2','3:3','4:4','2:2','3:3','4:4','3:3','4:4','4:4'])", function () {
+    Test.strictEqual(
+      points([
+        "1:1",
+        "2:2",
+        "3:3",
+        "4:4",
+        "2:2",
+        "3:3",
+        "4:4",
+        "3:3",
+        "4:4",
+        "4:4",
+      ]),
+      10
+    );
+  });
+  it("Testing for points(['0:1','0:2','0:3','0:4','1:2','1:3','1:4','2:3','2:4','3:4'])", function () {
+    Test.strictEqual(
+      points([
+        "0:1",
+        "0:2",
+        "0:3",
+        "0:4",
+        "1:2",
+        "1:3",
+        "1:4",
+        "2:3",
+        "2:4",
+        "3:4",
+      ]),
+      0
+    );
+  });
+  it("Testing for points(['1:0','2:0','3:0','4:0','2:1','1:3','1:4','2:3','2:4','3:4'])", function () {
+    Test.strictEqual(
+      points([
+        "1:0",
+        "2:0",
+        "3:0",
+        "4:0",
+        "2:1",
+        "1:3",
+        "1:4",
+        "2:3",
+        "2:4",
+        "3:4",
+      ]),
+      15
+    );
+  });
+  it("Testing for points(['1:0','2:0','3:0','4:4','2:2','3:3','1:4','2:3','2:4','3:4'])", function () {
+    Test.strictEqual(
+      points([
+        "1:0",
+        "2:0",
+        "3:0",
+        "4:4",
+        "2:2",
+        "3:3",
+        "1:4",
+        "2:3",
+        "2:4",
+        "3:4",
+      ]),
+      12
+    );
+  });
+});
--- Cafe/twoSum.js
+++ Cafe/twoSum.js
@@ -24,7 +24,7 @@
 var twoSum = function(nums, target) {
     let map = new Map;
     for (var i = 0; i < nums.length; i++) {
-        debugger;
+      
         let complement = target - nums[i];
         if (map.has(complement)) {
             return [map.get(complement), i]
