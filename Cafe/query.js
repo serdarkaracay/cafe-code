226d27de78eb8111f3257232db88d30a407d62f8
Serdar Karacay <csharp221@gmail.com>
2020-07-16 13:25:06 +0300

    query fın

---
+++ Cafe/query.js
@@ -0,0 +1,19 @@
+class Query {
+  constructor() {
+    this.data = [];
+    this.funcPredicateSelect = (i) => i;
+  }
+  select(funcPredicate = (f) => f) {
+    this.funcPredicateSelect = funcPredicate;
+    return this;
+  }
+  from(data) {
+    this.data = data;
+    return this;
+  }
+  execute() {
+    return this.data.map(this.funcPredicateSelect);
+  }
+}
+
+module.exports = () => new Query();
