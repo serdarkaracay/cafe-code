4dda39e614086367be934063079b366d5c5559ef
Serdar Karacay <csharp221@gmail.com>
2020-07-29 23:21:02 +0300

    groupby function test

--- Cafe/query.js
+++ Cafe/query.js
@@ -2,6 +2,8 @@ class Query {
   constructor() {
     this.data = [];
     this.funcPredicateSelect = (i) => i;
+    this.funcPredicateWhere = (i) => true;
+    this.funcPredicateGroupBy = null;
   }
   select(funcPredicate = (f) => f) {
     this.funcPredicateSelect = funcPredicate;
@@ -11,8 +13,36 @@ class Query {
     this.data = data;
     return this;
   }
+
+  where(funcPredicateWhere = (i) => true) {
+    this.funcPredicateWhere = funcPredicateWhere;
+    return this;
+  }
+
+  groupBy(...funcPredicateGroupBy) {
+    this.funcPredicateGroupBy = funcPredicateGroupBy;
+    return this;
+  }
   execute() {
-    return this.data.map(this.funcPredicateSelect)
+    let data = this.data;
+    data = data.filter(this.funcPredicateWhere);
+
+    if (this.funcPredicateGroupBy) {
+      const hash = {};
+      const groupArray = [];
+
+      data.forEach((i) => {
+        const name = this.funcPredicateGroupBy(i);
+        hash[name] = hash[name] || [];
+        hash[name].push(i);
+      });
+      data = Object.keys(hash).map((name) => {
+        return [name, hash[name]];
+      });
+    }
+    data = data.map(this.funcPredicateSelect);
+
+    return data;
   }
 }
 
